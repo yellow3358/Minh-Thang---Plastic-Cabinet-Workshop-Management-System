b23cc5d7adf675a4c23e841092d192ccc10cc65c
Đức Anh <zinka1379@gmail.com>
2026-02-26 21:29:26 +0700

    feat: setup routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "@/components/ui/sonner";
+
+import Login from "./pages/Login";
+import HomePage from "./pages/HomePage";
+import UserDetail from "./pages/UserDetail";
+import ForgotPassword from "./pages/ForgotPassword";
+import VerifyEmail from "./pages/VerifyEmail";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Toaster position="top-center" richColors closeButton duration={3500} />
+
+      <Routes>
+        {/* ========== PUBLIC ROUTES ========== */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/verify-email" element={<VerifyEmail />} />
+        <Route path="/user/:id" element={<UserDetail />} />
+
+        {/* ========== 404 ========== */}
+        <Route
+          path="*"
+          element={
+            <div className="flex items-center justify-center h-screen text-xl font-bold font-sans">
+              404 - Page Not Found
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
